db808f2e0ded0fb2e9e664473a1e43230c978cc0
Michel Hardy <michel.robin.hardy@gmail.com>
2021-05-22 12:03:41 +0200

    Iteration1

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,29 @@
+
+axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+    .then(response => {
+        printTheChart(response.data);
+    })
+    .catch(err => console.log(err))
+
+function printTheChart(stockData) {
+    const dailyData = stockData['Time Series (Daily)'];
+
+    const stockDates = Object.keys(dailyData);
+    const stockPrices = stockDates.map(date => dailyData[date]['4. close']);
+
+    const ctx = document.getElementById('my-chart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: stockDates,
+            datasets: [
+                {
+                    label: 'Stock Chart',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: stockPrices
+                }
+            ]
+        }
+    }); // closes chart = new Chart()
+} // closes printTheChart()
