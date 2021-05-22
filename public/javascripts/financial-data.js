e4b474debf048cbe061477a7153348589171eee3
Michel Hardy <michel.robin.hardy@gmail.com>
2021-05-22 12:59:28 +0200

    iteration3

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,15 +1,33 @@
 
-axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+// const start = document.querySelector("input[name='start']");
+// console.log(start);
+// const end = document.querySelector("input[name='end']");
+// console.log(end);
+
+const { response } = require("express")
+
+document.getElementById('btn').addEventListener('click', () => {
+    const start = document.getElementById('startDate').value
+    const end = document.getElementById('endDate').value
+    console.log("date", start, end)
+})
+
+// start.onclick = function () {
+//     console.log(start.value)
+// }
+
+axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
     .then(response => {
         printTheChart(response.data);
     })
     .catch(err => console.log(err))
 
 function printTheChart(stockData) {
-    const dailyData = stockData['Time Series (Daily)'];
+
+    const dailyData = stockData["bpi"];
 
     const stockDates = Object.keys(dailyData);
-    const stockPrices = stockDates.map(date => dailyData[date]['4. close']);
+    const stockPrices = stockDates.map(date => dailyData[date]);
 
     const ctx = document.getElementById('my-chart').getContext('2d');
     const chart = new Chart(ctx, {
@@ -27,3 +45,15 @@ function printTheChart(stockData) {
         }
     }); // closes chart = new Chart()
 } // closes printTheChart()
+
+function filter(start, end) {
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
+        .then(responseFromApi => {
+            const dailyData = responseFromApi.data.bpi
+            const stockDates = Object.keys(dailyData);
+            const stockPrices = stockDates.map(date => dailyData[date]);
+            printTheChart(stockDates, stockPrices)
+
+        })
+
+}
